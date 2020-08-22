e9d158cb698d914e0d000936ece134f821e664f2
Marc Westermann <westermann.m@gmail.com>
2020-08-22 20:40:15 +0200

    Initial Commit

---
+++ beat-tap WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beat-tap WatchKit Extension
+//
+//  Created by Marc Westermann on 22/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beat-tap WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  beat-tap WatchKit Extension
+//
+//  Created by Marc Westermann on 22/08/2020.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
